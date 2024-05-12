33a73de7a918d9e80cd5f6d5ceb2224b9573ac30
procomjet <procomjet@gmail.com>
2024-05-13 01:06:15 +0900

    5. chart 구현, react-hydration-error

--- src/app/(main)/analysis/page.tsx
+++ src/app/(main)/analysis/page.tsx
@@ -2,14 +2,20 @@
 
 import Chart from "./_components/Chart";
 import DayPicker from "./_components/DayPicker";
+// import dynamic from "next/dynamic";
+
+// const Chart = dynamic(() => import("./_components/Chart"), { ssr: false });
+// const DayPicker = dynamic(() => import("./_components/DayPicker"), {
+//   ssr: false,
+// });
 
 export default function page() {
   return (
     <div className="w-[414px] h-[736px] relative overflow-hidden bg-[#d6d7ef]">
-      <div className="w-[356.68px] h-[200px]">
-        <DayPicker />
+      <div className="w-[356.68px] h-[100px]">{/* <DayPicker /> */}</div>
+      <div className="w-[400px] h-[500px]">
+        <Chart />
       </div>
-      <div className="w-[400px] h-[500px]">{/* <Chart /> */}</div>
     </div>
   );
 }
